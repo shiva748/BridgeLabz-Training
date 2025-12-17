387146e1aa0724ad5e148efa7e5fcfe45486022e
Shiva <Shivagautam2002@gmail.com>
2025-12-17 21:03:01 +0530

    [shiva] Add. Calculate perimeter of a rectangle

---
+++ core-java-practice/gcr-codebase/programming-elements/perimeter.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class perimeter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter length:- ");
+        int length = sc.nextInt();
+        System.out.print("Please enter breadth:- ");
+        int breadth = sc.nextInt();
+        System.out.printf("Perimeter of rectangle is:- %d", 2 * (length + breadth));
+        sc.close();
+    }
+}
