53ae1fb1d30197b1a5bc8d54277c82c04c6e1607
Shiva <Shivagautam2002@gmail.com>
2025-12-17 20:47:58 +0530

    [shiva] Add. Addition of two number

---
+++ core-java-practice/gcr-codebase/programming-elements/add.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class add {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter first Number:- ");
+        int a = sc.nextInt();
+        System.out.print("Please enter second Number:- ");
+        int b = sc.nextInt();
+        System.out.println("Sum:- "+(a+b));
+        sc.close();
+    }
+}
