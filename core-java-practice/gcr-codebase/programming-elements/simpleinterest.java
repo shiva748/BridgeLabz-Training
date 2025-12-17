cf77c075eed88036ae8518945ecbe13dc31f92fa
Shiva <Shivagautam2002@gmail.com>
2025-12-17 21:00:37 +0530

    [shiva] Add. Calculate simple interest

---
+++ core-java-practice/gcr-codebase/programming-elements/simpleinterest.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class simpleinterest {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter Principal amount:- ");
+        int amt = sc.nextInt();
+        System.out.print("Please enter Rate of interest:- ");
+        float rate = sc.nextFloat();
+        System.out.print("Please enter Time period in years:- ");
+        int time = sc.nextInt();
+        float si = (amt * rate * time) / 100;
+        System.out.printf("Simple Interest is:- %.2f", si);
+        sc.close();
+    }
+}
