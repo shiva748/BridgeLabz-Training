7d1b2b46583e020bb5ad07a746a05089f3babf01
Shiva <Shivagautam2002@gmail.com>
2025-12-18 11:50:51 +0530

    [shiva] Add. Implement discount calculation program

---
+++ core-java-practice/gcr-codebase/programming-elements/discount.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class discount {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Please enter the fee amount INR:- ");
+            int amt = sc.nextInt();
+            System.out.print("Please enter discount %:- ");
+            double dis = sc.nextDouble();
+            System.out.printf("The discount amount is INR %.2f and final discounted fee is INR %.2f%n", (double)(amt*dis)/100, (double)(amt*(100-dis))/100);
+            sc.close();
+    }
+}
