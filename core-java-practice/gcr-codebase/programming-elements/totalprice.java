a5a68056cd8da89d995c67efbcb5d5a553dc41dc
Shiva <Shivagautam2002@gmail.com>
2025-12-18 14:52:41 +0530

    [shiva] Add. Implement program to calculate total price based on unit price and quantity

---
+++ core-java-practice/gcr-codebase/programming-elements/totalprice.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class totalprice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter price of a unit:- ");
+        float price = sc.nextFloat();
+        System.out.print("Please enter total unit's to be bought:- ");
+        int unit = sc.nextInt();
+        System.out.printf("The total cost of buying %d units @ %.2f/unit is %.2f", unit, price, unit*price);
+        sc.close();
+    }   
+}
