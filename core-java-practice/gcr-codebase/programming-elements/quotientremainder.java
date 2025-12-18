b18a696ab58f116ff3dcec841550ce08f884a17b
Shiva <Shivagautam2002@gmail.com>
2025-12-18 15:08:14 +0530

    [shiva] Add. Implement program to calculate quotient and remainder of two numbers

---
+++ core-java-practice/gcr-codebase/programming-elements/quotientremainder.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class quotientremainder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter two numbers:- ");
+        int a = sc.nextInt(), b = sc.nextInt();
+        System.out.printf("For divison of %d/%d the quotient is %d and remainder is %d", a, b, a/b, a%b);
+        sc.close();
+    }
+}
