0d1eceeaff43d4e581776a2f60304300a34f0810
Shiva <Shivagautam2002@gmail.com>
2025-12-18 14:47:45 +0530

    [shiva] Add. Implement program to convert distance from feet to yards and miles

---
+++ core-java-practice/gcr-codebase/programming-elements/distanceconversion.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class distanceconversion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter you distance in feet's:- ");
+        float feet = sc.nextFloat();
+        System.out.printf("The distance of %.2f feet is equivalent to %.2f yard and %.2f mile", feet, feet/3, feet/(3*1760));
+        sc.close();
+    }
+}
