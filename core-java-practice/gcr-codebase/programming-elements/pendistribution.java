564d8afe908082dfffc43a97ca95d6a47f382f80
Shiva <Shivagautam2002@gmail.com>
2025-12-18 11:39:26 +0530

    [shiva] Add. Implement pen distribution calculation program

---
+++ core-java-practice/gcr-codebase/programming-elements/pendistribution.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class pendistribution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter total number of pen's:- ");
+        int pen = sc.nextInt();
+        System.out.print("Please enter total student's:- ");
+        int stu = sc.nextInt();
+        System.out.printf("The Pen Per Student is %d and the remaning pen not distributed is %d%n", pen/stu, pen%stu);
+        sc.close();
+    }
+}
