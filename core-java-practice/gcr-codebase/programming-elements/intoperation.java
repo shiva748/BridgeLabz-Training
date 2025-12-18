a0f68e8f8d291cf82032b5508de77de621528aac
Shiva <Shivagautam2002@gmail.com>
2025-12-18 15:08:35 +0530

    [shiva] Add. Implement program to perform various integer operations on three numbers

---
+++ core-java-practice/gcr-codebase/programming-elements/intoperation.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class intoperation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter three numbers:- ");
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+        System.out.printf("The results of Int Operations are %d, %d, %d, and %d", a+(b*c),(a*b)+c,c+(a/b),(a%b)+c);
+        sc.close();
+    }
+}
