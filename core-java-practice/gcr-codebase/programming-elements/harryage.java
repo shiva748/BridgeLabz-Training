14ee452c3ea2b33d80898e7c4376e9bca7324dcf
Shiva <Shivagautam2002@gmail.com>
2025-12-18 10:52:57 +0530

    [shiva] Add. Implement program to calculate Harry's age based on birth year

---
+++ core-java-practice/gcr-codebase/programming-elements/harryage.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class harryage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter birtyear:- ");
+        int birth = sc.nextInt();
+        System.out.println("Harry's age in 2024 is:- "+(2024-birth));
+        sc.close(); 
+    }
+}
