0dbfcd19bdd9f3d26dc97fac8d72dfe9b68afab2
Shiva <Shivagautam2002@gmail.com>
2025-12-18 11:34:08 +0530

    [shiva] Add. Implement profit and loss calculation program

---
+++ core-java-practice/gcr-codebase/programming-elements/profitloss.java
@@ -0,0 +1,13 @@
+import  java.util.Scanner;
+
+public class profitloss {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter C.P:- ");
+        int cp = sc.nextInt();
+        System.out.print("Please enter S.P:- ");
+        int sp = sc.nextInt();
+        System.out.printf("The Cost Price is INR %d and Selling Price is INR %d%nThe Profit is INR %d and the Profit Percentage is %.2f",cp, sp, sp-cp, (double)(sp-cp)/cp*100);
+        sc.close();
+    }
+}
