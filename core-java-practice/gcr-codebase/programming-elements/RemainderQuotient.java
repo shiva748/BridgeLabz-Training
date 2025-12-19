b2106199043313015b8c12025d0b164bc0b12b51
Shiva <Shivagautam2002@gmail.com>
2025-12-19 11:25:31 +0530

    [shiva] Refactor. fixing naming convention of remaninder and quotient calculator program

--- core-java-practice/gcr-codebase/programming-elements/RemainderQuotient.java
+++ core-java-practice/gcr-codebase/programming-elements/RemainderQuotient.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class quotientremainder {
+public class RemainderQuotient {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Please enter two numbers:- ");
