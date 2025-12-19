604806eafa71da2a296170ead3a57004ef0671c8
Shiva <Shivagautam2002@gmail.com>
2025-12-19 11:26:43 +0530

    [shiva] Refactor. fixing naming convention of square side from perimeter program

--- core-java-practice/gcr-codebase/programming-elements/SquareSide.java
+++ core-java-practice/gcr-codebase/programming-elements/SquareSide.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class side {
+public class SquareSide {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Please enter perimeter of square:- ");
