5e9d6a11f77831c8133a05fbbe02d0650c0a9731
Shiva <Shivagautam2002@gmail.com>
2025-12-19 11:30:02 +0530

    [shiva] Refactor. fixing naming convention of temprature conversion program

--- core-java-practice/gcr-codebase/programming-elements/ConvertTemprature.java
+++ core-java-practice/gcr-codebase/programming-elements/ConvertTemprature.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class tempratureconvert {
+public class ConvertTemprature {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
