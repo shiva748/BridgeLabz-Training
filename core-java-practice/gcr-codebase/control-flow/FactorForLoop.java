3607493867c4dc9ceeb0bdd43304346429cfa182
Shiva <Shivagautam2002@gmail.com>
2025-12-19 20:31:28 +0530

    [shiva] Add. program to find factor of a number using for loop

---
+++ core-java-practice/gcr-codebase/control-flow/FactorForLoop.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FactorForLoop {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Please enter a number:- ");
+		int num = scanner.nextInt();
+		for (int i = 1; i <= num; i++) {
+			if (i == num) {
+				System.out.println(num);
+			} else if (num % i == 0) {
+				System.out.printf("%d, ", i);
+			}
+		}
+		scanner.close();
+
+	}
+
+}
