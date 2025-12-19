32f8ac0377faafa8365133d1101a983e9d2c7d74
Shiva <Shivagautam2002@gmail.com>
2025-12-19 11:02:50 +0530

    [shiva] Refactor. fixing naming convention of Km to mile conversion program

--- core-java-practice/gcr-codebase/programming-elements/ConversionKmToMile.java
+++ core-java-practice/gcr-codebase/programming-elements/ConversionKmToMile.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class convertkmtomile {
+public class ConversionKmToMile {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
