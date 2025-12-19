99b9599eb8662e51c4aebcc1aeb1946f1c0e4686
Shiva <Shivagautam2002@gmail.com>
2025-12-19 11:15:07 +0530

    [shiva] Refactor. fixing naming convention of harry age calculation program

--- core-java-practice/gcr-codebase/programming-elements/AgeOfHarry.java
+++ core-java-practice/gcr-codebase/programming-elements/AgeOfHarry.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
-public class harryage {
+public class AgeOfHarry
+ {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter birtyear:- ");
