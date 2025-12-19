a4bc8f6e509403e8a0b556f7215e0e38bff97882
Shiva <Shivagautam2002@gmail.com>
2025-12-19 21:45:31 +0530

    [shiva] Add. calculate power of a number using while loop

---
+++ core-java-practice/gcr-codebase/control-flow/PowerWhileLoop.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PowerWhileLoop {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Please enter a number:- ");
+		int num = scanner.nextInt();
+		System.out.print("Please enter power:- ");
+		int pow = scanner.nextInt();
+		int res = 1;
+		for (int i = 0; i < pow; i++) {
+			res *= num;
+		}
+		System.out.println(res);
+		scanner.close();
+	}
+
+}
