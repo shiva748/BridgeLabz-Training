c25e1d3d03e124ea302ee2573eb5cd16203f8caa
Shiva <Shivagautam2002@gmail.com>
2025-12-19 11:32:23 +0530

    [shiva] Refactor. fixing naming convention of triangle area calculation program

--- core-java-practice/gcr-codebase/programming-elements/AreaOfTriangle.java
+++ core-java-practice/gcr-codebase/programming-elements/AreaOfTriangle.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class trianglearea {
+public class AreaOfTriangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Please enter base of triangle:- ");
