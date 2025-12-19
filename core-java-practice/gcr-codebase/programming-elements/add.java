262b8de5f63d8d0a5dc719205a3849a4e9c9d2a5
Shiva <Shivagautam2002@gmail.com>
2025-12-19 10:48:33 +0530

    [shiva] Refactor. fix naming conventions

--- core-java-practice/gcr-codebase/programming-elements/add.java
+++ core-java-practice/gcr-codebase/programming-elements/add.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class add {
+public class Add {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Please enter first Number:- ");
