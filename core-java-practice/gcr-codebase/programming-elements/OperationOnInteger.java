11fff1a1200b9c98182866ce25666ce9f862ba48
Shiva <Shivagautam2002@gmail.com>
2025-12-19 11:18:55 +0530

    [shiva] Refactor. fixing naming convention of basic operation on integer program

--- core-java-practice/gcr-codebase/programming-elements/OperationOnInteger.java
+++ core-java-practice/gcr-codebase/programming-elements/OperationOnInteger.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class intoperation {
+public class OperationOnInteger {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Please enter three numbers:- ");
