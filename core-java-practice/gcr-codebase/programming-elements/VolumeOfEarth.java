8daa91a5f9341888d6fd0c48e4c324e63b69129c
Shiva <Shivagautam2002@gmail.com>
2025-12-19 11:11:06 +0530

    [shiva] Refactor. fixing naming convention of earth volume calculator program

--- core-java-practice/gcr-codebase/programming-elements/VolumeOfEarth.java
+++ core-java-practice/gcr-codebase/programming-elements/VolumeOfEarth.java
@@ -1,4 +1,4 @@
-public class earthvolume {
+public class VolumeOfEarth {
     public static void main(String[] args) {
         int radius = 6378;
         System.out.printf("The volume of earth in cubic kilometers is %.2f and cubic miles is %.2f", (double)(4/3*Math.PI*Math.pow(radius, 3)), (double)(4/3*Math.PI*Math.pow(radius*0.621371, 3)));
