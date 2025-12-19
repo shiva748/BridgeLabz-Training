31321f2cbfcc0b31cfe6ef65fb5bdf71db5a98ec
Shiva <Shivagautam2002@gmail.com>
2025-12-19 11:06:00 +0530

    [shiva] Refactor. fixing naming convention of University discounted fee calculator program

--- core-java-practice/gcr-codebase/programming-elements/UniversityDiscount.java
+++ core-java-practice/gcr-codebase/programming-elements/UniversityDiscount.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class DiscountCalculator {
+public class UniversityDiscount {
     public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             System.out.print("Please enter the fee amount INR:- ");
