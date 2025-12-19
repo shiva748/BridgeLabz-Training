08572b73659ad09418fae732c41e86d14fd13021
Shiva <Shivagautam2002@gmail.com>
2025-12-19 11:28:09 +0530

    [shiva] Refactor. fixing naming convention of simple intrest calculation program

--- core-java-practice/gcr-codebase/programming-elements/SimpleIntrestCalculator.java
+++ core-java-practice/gcr-codebase/programming-elements/SimpleIntrestCalculator.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class simpleinterest {
+public class SimpleIntrestCalculator {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
