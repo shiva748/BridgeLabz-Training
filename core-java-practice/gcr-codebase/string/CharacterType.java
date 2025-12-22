075bc0c96645558fdd43963eb23c442ca262b7f4
Shiva <Shivagautam2002@gmail.com>
2025-12-22 20:25:27 +0530

    [shiva] Add. program to print character type of each character in a string

---
+++ core-java-practice/gcr-codebase/string/CharacterType.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class CharacterType {
+    public static String checkCharacter(char ch) {
+        if (ch >= 'A' && ch <= 'Z') {
+            ch = (char) (ch + 32);
+        }
+        if (ch >= 'a' && ch <= 'z') {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                return "Vowel";
+            } else {
+                return "Consonant";
+            }
+        }
+        return "Not a Letter";
+    }
+    public static String[][] findCharacterTypes(String text) {
+        String[][] result = new String[text.length()][2];
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            result[i][0] = String.valueOf(ch);
+            result[i][1] = checkCharacter(ch);
+        }
+        return result;
+    }
+    public static void displayTable(String[][] data) {
+        System.out.println("Character\tType");
+        System.out.println("----------------------");
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(data[i][0] + "\t\t" + data[i][1]);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter text");
+        String text = scanner.nextLine();
+        String[][] result = findCharacterTypes(text);
+        displayTable(result);
+        scanner.close();
+    }
+}
