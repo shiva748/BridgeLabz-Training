6edb2d04ac180b6fb5984a83471102e06f35196b
Shiva <Shivagautam2002@gmail.com>
2025-12-26 10:28:36 +0530

    [shiva] Add. Implemented library management using Book and Author classes

--- oops-practice/gcr-codebase/inheritance/Library.java
+++ oops-practice/gcr-codebase/inheritance/Library.java
@@ -1,4 +1,36 @@
+class Book {
+	String title;
+	int publicationYear;
+
+	public void displayInfo() {
+		System.out.println("Book Title: " + title);
+		System.out.println("Publication Year: " + publicationYear);
+	}
+}
+
+class Author extends Book {
+	String name;
+	String bio;
+
+	@Override
+	public void displayInfo() {
+		System.out.println("Book Title: " + title);
+		System.out.println("Publication Year: " + publicationYear);
+		System.out.println("Author Name: " + name);
+		System.out.println("Author Bio: " + bio);
+	}
+}
 
 public class Library {
+	public static void main(String[] args) {
+		Book book = new Author();
+		book.title = "Java Fundamentals";
+		book.publicationYear = 2023;
 
+		Author author = (Author) book;
+		author.name = "Ravi Kumar";
+		author.bio = "Software Developer and Java Trainer";
+
+		book.displayInfo();
+	}
 }
