1927b4741b93ef584a53d093e1f4b711cda97912
Shiva <Shivagautam2002@gmail.com>
2026-01-03 10:32:16 +0530

    [shiva] Refactor. preserve element order when handling odd zero count in balance logic.

--- review/ZeroBalance.java
+++ review/ZeroBalance.java
@@ -28,24 +28,34 @@ public class ZeroBalance {
 			i++;
 		}
 	}
-    public static void shiftBy(int arr[], int by, int nonzero){
-        int i = nonzero-1;
-        while(i >= 0){
-            arr[i+by] = arr[i];
-            arr[i] = 0;
-            i--;
-        }
-    }
+
+	public static void shiftBy(int arr[], int by, int nonzero) {
+		int i = nonzero - 1;
+		while (i >= 0) {
+			arr[i + by] = arr[i];
+			arr[i] = 0;
+			i--;
+		}
+	}
+
 	public static void balance(int arr[]) {
 		int count = count(arr);
-		int start = 0, itrator = 0;
+
 		movezerotoend(arr);
-		shiftBy(arr, count/2, arr.length - count);
-		if(count %2 != 0){
-		    arr[arr.length - (count/2 + 1)] = arr[arr.length/2];
-		    arr[arr.length/2] = 0;
+
+		shiftBy(arr, count / 2, arr.length - count);
+
+		if (count % 2 != 0) {
+			int mid = arr.length / 2;
+			int end = arr.length - (count / 2) - 1;
+
+			for (int i = end; i >= mid; i--) {
+				arr[i + 1] = arr[i];
+			}
+			arr[mid] = 0;
 		}
-        System.out.println(Arrays.toString(arr));
+
+		System.out.println(Arrays.toString(arr));
 	}
 
 	public static void main(String[] args) {
@@ -53,11 +63,12 @@ public class ZeroBalance {
 		System.out.print("Please enter size of array: ");
 		int size = scanner.nextInt();
 		int arr[] = new int[size];
+
 		for (int i = 0; i < size; i++) {
 			arr[i] = scanner.nextInt();
 		}
+
 		balance(arr);
 		scanner.close();
 	}
-
 }
