4ca86513b5e5fa07c50e4fe094922d7dbfec632a
Shiva <Shivagautam2002@gmail.com>
2026-01-31 07:00:48 +0530

    [shiva] Add. Implemented TemperatureSystem class with temperature threshold alert functionality.

--- java8-features-practice/gcr-codebase/functional-interfaces/TemperatureSystem.java
+++ java8-features-practice/gcr-codebase/functional-interfaces/TemperatureSystem.java
@@ -1,4 +1,27 @@
+import java.util.function.Predicate;
 
 public class TemperatureSystem {
 
+	public static void main(String[] args) {
+
+		double threshold = 40.0;
+
+		Predicate<Double> alertCheck = temp -> temp > threshold;
+
+		checkTemperature(35.5, alertCheck);
+		checkTemperature(42.0, alertCheck);
+		checkTemperature(39.0, alertCheck);
+		checkTemperature(45.8, alertCheck);
+	}
+
+	public static void checkTemperature(double temperature, Predicate<Double> alertCheck) {
+
+		if (alertCheck.test(temperature)) {
+			System.out.println("Temperature " + temperature + " crossed threshold.");
+			System.out.println("ALERT triggered.");
+			return;
+		}
+
+		System.out.println("Temperature " + temperature + " is normal.");
+	}
 }
